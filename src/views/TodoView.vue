8629e3331728dd2c8cc6594b31793d6925b79f6a
rimo32d <rimo32d@gmail.com>
2022-12-01 18:54:45 +0900

    ボタンパーツ読み込み、課題追加処理、削除機能追加

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -3,23 +3,36 @@
     <h2>ToDoリスト</h2>
     <form @submit.prevent>
       <div class="l-flex">
-        <div class="">
-          名前
-        </div>
-        <input type="text" v-model="todoText"/>
+        <div class="">名前</div>
+        <input type="text" v-model="todoText" />
       </div>
-      <button class="" type="button" @click="addItem()">追加</button>
+      <TheButton @click="addItem()" text="追加" />
     </form>
-    <div class="">{{ todoList }}</div>
+    <ul>
+      <li v-for="(todo, index) in todoList" :key="todo">
+        <input type="checkbox" v-model="todo.isDone" />
+        <span v-bind:class="{ done: todo.isDone }">{{ todo.item }}</span>
+        <TheButton @onClick="deleteButton(index)" text="削除" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue';
-  const todoList = ref([])
-  const todoText = ref('')
-  const addItem = () => {
-    todoList.value.push(todoText.value)
-    todoText.value = ''
-  }
+import { ref } from "vue";
+import TheButton from "../components/TheButton.vue";
+const todoList = ref([]);
+const todoText = ref("");
+const addItem = () => {
+  if( todoText.value === '' ) return 
+  let todo = {
+    item: todoText.value,
+    isDone: false,
+  };
+  todoList.value.push(todo);
+  todoText.value = "";
+};
+let deleteButton = (index) => {
+  todoList.value.splice(index, 1);
+};
 </script>
