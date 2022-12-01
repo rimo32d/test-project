dc3e34e53234ea6d7a235d893c868ecf33a8c7e0
rimo32d <rimo32d@gmail.com>
2022-12-01 09:34:08 +0900

    リストに入力した値を入れる

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
     <h2>ToDoリスト</h2>
-    <form>
-      <input type="text" />
-      <button>追加</button>
+    <form @submit.prevent>
+      <div class="l-flex">
+        <div class="">
+          名前
+        </div>
+        <input type="text" v-model="todoText"/>
+      </div>
+      <button class="" type="button" @click="addItem()">追加</button>
     </form>
+    <div class="">{{ todoList }}</div>
   </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue';
+  const todoList = ref([])
+  const todoText = ref('')
+  const addItem = () => {
+    todoList.value.push(todoText.value)
+    todoText.value = ''
+  }
+</script>
