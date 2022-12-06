1c3936460f1a1732394357375183bfa7212a2432
rimo32d <rimo32d@gmail.com>
2022-12-06 18:46:25 +0900

    headerをコンポーネント分け

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <nav>
-    <a href="/">Home</a> |
-    <a href="/parts">parts</a> |
-    <a href="/about">about</a> |
-    <a href="/todo">todoリスト</a>
-  </nav>
+  <TheHeader />
   <router-view/>
 </template>
+<script setup>
+  import TheHeader from './components/TheHeader.vue';
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <nav>
+      <a href="/">Home</a> | <a href="/parts">parts</a> |
+      <a href="/about">about</a> |
+      <a href="/todo">todoリスト</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+}
+</script>
