15b86a2607ced86d156d182e4a7cf5e42bbd9df4
rimo32d <rimo32d@gmail.com>
2023-01-12 19:44:59 +0900

    これまでの記述を削除

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,26 +1,4 @@
 <template>
-  <!-- <div id="app">
-    <h2>ToDoリスト</h2>
-    <form @submit.prevent>
-      <div class="l-flex">
-        <div class="">名前</div>
-        <input type="text" v-model="todoText" />
-      </div>
-      <TheButton @click="addItem()" text="追加" />
-    </form>
-    <ul>
-      <li v-for="(todo, index) in todoList" :key="todo">
-        <input type="checkbox" v-model="todo.isDone" />
-        <span v-bind:class="{ done: todo.isDone }">
-          {{ todo.item }}
-        </span>
-        <TheButton @onClick="editText(index)" text="編集" />
-
-        <input type="text" v-if="todo.isOpen" v-model="todo.item" />
-        <TheButton @onClick="deleteButton(index)" text="削除" />
-      </li>
-    </ul>
-  </div> -->
   <div id="trello" class="l-flex" style="align-items: flex-start">
     <div v-for="(list, index) in lists" :key="index" class="c-list">
       <div class="c-list--title">{{ list.name }}</div>
@@ -50,30 +28,6 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-// import TheButton from "../components/TheButton.vue";
-// const todoList = ref([]);
-// const todoText = ref("");
-// const addItem = () => {
-//   if (todoText.value === "") return;
-//   let todo = {
-//     item: todoText.value,
-//     isDone: false,
-//     isOpen: false,
-//   };
-//   todoList.value.push(todo);
-//   todoText.value = "";
-// };
-// let deleteButton = (index) => {
-//   todoList.value.splice(index, 1);
-// };
-// let editText = (index) => {
-//   let editFrag = todoList.value[index].isOpen;
-//   if (editFrag) {
-//     todoList.value[index].isOpen = false;
-//   } else {
-//     todoList.value[index].isOpen = true;
-//   }
-// };
 let lists = ref([
   {
     id: 1,
